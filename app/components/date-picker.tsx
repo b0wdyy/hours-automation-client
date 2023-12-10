5674025ee6bfdbc3f4198f65daec26ff488c056c
b0wdyy <vandaelbodhi@hotmail.com>
2023-12-10 22:23:19 +0100

    feat: date picker can select dates

--- app/components/date-picker.tsx
+++ app/components/date-picker.tsx
@@ -37,7 +37,7 @@ export function DatePicker() {
   const firstDayCurrentMonth = parse(currentMonth, 'MMM-yyyy', new Date())
   const {
     dispatch,
-    state: { dates },
+    state: { dates, token },
   } = useGlobalState()
 
   const days = eachDayOfInterval({
@@ -61,6 +61,47 @@ export function DatePicker() {
       type: 'add_date',
       value: day,
     })
+  }
+
+  async function onSendToTeamleader() {
+    const data = dates.map((date) => {
+      const dateFormatted = format(date, 'dd/MM/yyyy')
+      const encodedDate = encodeURIComponent(dateFormatted)
+
+      return fetch(
+        'https://focus.teamleader.eu/backend/client-api/save_timetracking.php',
+        {
+          credentials: 'include',
+          headers: {
+            Origin: 'https://focus.teamleader.eu',
+            Authorization: `Bearer ${token}`,
+            'User-Agent':
+              'Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0',
+            Accept: 'application/json',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'X-TLC': '1',
+            'TL-Strategy': 'Content-replace',
+            'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+          },
+          referrer:
+            'https://focus.teamleader.eu/timesheets.php?year=2023&month=12&week=0&day=10&view=week',
+          body: `popup_type=timetracking&subject_type=general&subject_id=-1&=&corc_id_type=company&corc_id=514ca5ba-1c4a-0ed5-be70-7e82f27d118b&corc_id_uuid=514ca5ba-1c4a-0ed5-be70-7e82f27d118b&optional_contact_person_id=-2&invoiceable=1&project_id=2199054&milestone_id=9157274&todo_kind_id=06f3794a-c4ff-08e2-815e-223a8cf2eead&call_outcome_status=-1&title=&todo_suggested_id=30568605&done=0&date_start=${encodedDate}&start_time=08%3A00&add_report=0&suggest_todos=1&show_task_finished_slider=1&`,
+          method: 'POST',
+          mode: 'cors',
+        },
+      )
+    })
+
+    try {
+      await Promise.allSettled(data)
+      console.log('All done!')
+      alert('Check your timesheets')
+    } catch (e: unknown) {
+      console.error(e)
+    }
   }
 
   return (
@@ -135,6 +176,13 @@ export function DatePicker() {
           </div>
         ))}
       </div>
+      <button
+        className="px-4 py-2 rounded-lg bg-slate-900 text-white disabled:bg-slate-300 disabled:cursor-not-allowed"
+        onClick={onSendToTeamleader}
+        disabled={!dates.length}
+      >
+        Send to teamleader
+      </button>
     </>
   )
 }
